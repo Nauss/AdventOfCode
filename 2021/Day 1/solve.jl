f21212846325dcc936af3e285496c95d802c0379
nauss <bpommier@gmail.com>
2021-11-26 13:44:10 +0100

    fix: clean up code to get ready

--- 2021/Day 1/solve.jl
+++ 2021/Day 1/solve.jl
@@ -1,1 +1,6 @@
-split(open(f -> read(f, String),"data.txt"),"\r\n")
+using Pipe: @pipe
+
+rules = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+
+part1 = @pipe "./data.txt" |> readlines;
+println("data ", part1)
