d2180ff2965b86cfc3c2beda1a59a09848244c14
nauss <bpommier@gmail.com>
2021-12-01 09:26:51 +0100

    Day 1 🤙

--- 2021/Day 1/solve.jl
+++ 2021/Day 1/solve.jl
@@ -1,6 +1,26 @@
 using Pipe: @pipe
 
-rules = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+rawValues = @pipe "./data.txt" |> readlines;
+values = parse.(Int, rawValues);
 
-part1 = @pipe "./data.txt" |> readlines;
-println("data ", part1)
+diffs = similar(values);
+for i = 2:length(values)
+    diffs[i] = values[i] - values[i-1]
+end
+diffs[1] = diffs[2]
+part1 = count(x -> x > 0, diffs)
+println("part1 ", part1)
+
+sums = similar(values);
+for i = 3:length(values)
+    sums[i] = values[i] + values[i-1] + values[i-2]
+end
+sums[1] = sums[3]
+sums[2] = sums[3]
+diffs2 = similar(sums);
+for i = 2:length(sums)
+    diffs2[i] = sums[i] - sums[i-1]
+end
+diffs2[1] = 0
+part2 = count(x -> x > 0, diffs2)
+println("part2 ", part2)
