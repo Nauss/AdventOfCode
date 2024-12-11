018cf16a591a0714b89009e0f1bbf138cfa77a26
Bruno <bruno.pommier@orme.com>
2024-12-11 15:17:07 +0100

    Added a variant using functool

--- 2024/Day 11/main.py
+++ 2024/Day 11/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import time
+import functools
 
 
 def applyRules(value):
@@ -48,3 +49,36 @@ with path.open() as f:
     result = solve(line, cache, 75)
     print("part2: ", result)
     print("Runtime: ", time.time() - start)
+
+
+# @functools.lru_cache(maxsize=None)
+# def getCache(value, blinks):
+#     if blinks == 0:
+#         return 1
+
+#     applied = applyRules(value)
+#     result = getCache(applied[0], blinks - 1)
+#     if len(applied) > 1:
+#         result += getCache(applied[1], blinks - 1)
+#     return result
+
+
+# def solve(line, blinks):
+#     nbStones = 0
+#     for i in range(len(line)):
+#         nbStones += getCache(line[i], blinks)
+
+#     return nbStones
+
+
+# # Open data.txt file
+# path = Path(__file__).parent / "./data.txt"
+# with path.open() as f:
+#     # Read all lines
+#     line = [int(x) for x in [line.strip().split(" ") for line in f][0]]
+#     result = solve(line, 25)
+#     print("part1: ", result)
+#     start = time.time()
+#     result = solve(line, 75)
+#     print("part2: ", result)
+#     print("Runtime: ", time.time() - start)
