3cae90f80a759fc3c1e30bec3914491a026d7669
Bruno <bruno.pommier@orme.com>
2024-12-11 14:36:46 +0100

    Almost lost Day 10 😥

---
+++ 2024/Day 11/main.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import time
+
+
+def applyRules(value):
+    strValue = str(value)
+    if value == 0:
+        return [1]
+    if len(strValue) % 2 == 0:
+        return [
+            int(strValue[: len(strValue) // 2]),
+            int(strValue[len(strValue) // 2 :]),
+        ]
+    else:
+        return [value * 2024]
+
+
+def getCache(value, cache, blinks):
+    if (value, blinks) in cache:
+        return cache[(value, blinks)]
+    if blinks == 0:
+        return 1
+
+    applied = applyRules(value)
+    cache[(value, blinks)] = getCache(applied[0], cache, blinks - 1)
+    if len(applied) > 1:
+        cache[(value, blinks)] += getCache(applied[1], cache, blinks - 1)
+    return cache[(value, blinks)]
+
+
+def solve(line, cache, blinks):
+    nbStones = 0
+    for i in range(len(line)):
+        nbStones += getCache(line[i], cache, blinks)
+
+    return nbStones
+
+
+# Open data.txt file
+path = Path(__file__).parent / "./data.txt"
+with path.open() as f:
+    # Read all lines
+    line = [int(x) for x in [line.strip().split(" ") for line in f][0]]
+    cache = {}
+    result = solve(line, cache, 25)
+    print("part1: ", result)
+    start = time.time()
+    result = solve(line, cache, 75)
+    print("part2: ", result)
+    print("Runtime: ", time.time() - start)
